706d59f766f9043f43d49422b94358a143988d8b
Yashavi Prakash <yp3ez@virginia.edu>
2021-09-15 16:42:52 +0000

    removed gibbersh

--- src/homework/practice_1.lean
+++ src/homework/practice_1.lean
@@ -4,9 +4,6 @@ secret code name: groundhog
 Yashavi Prakash, yp3ez, github url: https://github.com/yashaviprakash/cs2120f21.git
 Jacqueline Chao, jc7rw, github url: https://github.com/arte-et-marte/my_cs2120f21.git
 
-sjdfklsjdfklsj
-
-sdfjslakd fd new change new change new change new change new change
 -/
 
 
