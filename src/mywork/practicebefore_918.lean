53c6c136595d262b6b51d85c6d2d9d2561ebaf57
Yashavi Prakash <yp3ez@virginia.edu>
2021-09-16 13:31:22 +0000

    new review file

---
+++ src/mywork/practicebefore_918.lean
@@ -0,0 +1,26 @@
+/- Hey everyone!-/
+
+/-Here I'll summarize and put practice problems for retaining
+the material from the past three weeks because there was definitely a lot to work with!-/
+
+/-lecture 1 review-/
+
+/-
+Reasoning:
+
+Inductive, Abductive, Deductive
+
+Inductive: 
+
+-derive generalized predictive models from
+given sets of observations, use a reasonable form of model
+(often linear, y = mx+b)
+-calibrate m and b coefficients to get a minimum-error fit of 
+line to data
+Is this form of reasoning *sound* or *not sound*
+
+
+
+
+
+-/
