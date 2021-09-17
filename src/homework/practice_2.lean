95f30d92c5b93a148dd6aeaa2376c2ecfc232c17
Yashavi Prakash <yp3ez@virginia.edu>
2021-09-17 20:55:42 +0000

    new changes

--- src/homework/practice_2.lean
+++ src/homework/practice_2.lean
@@ -6,24 +6,77 @@ in the sense that they identify all the axioms
 and/or theorems that you use.
 -/
 
-example : true := _
+example : true := true.intro
 
-example : false := _    -- trick question? why?
+/- 
+false is uninhabited proposition type, a type with no values at all, no way to construct a proof of false
+for an proposition to be false, there is no proof of it (true propositions have a truth to it)
+-/
+example : false :=    -- trick question? why? yes, bc there is no proof of false. 
 
+/- for any propposition P, P or P is true if and only if P is true-/
 example : ∀ (P : Prop), P ∨ P ↔ P := 
 begin
+    assume P, 
+    apply iff.intro _ _,
+    -- forward
+      assume porp, 
+      apply or.elim porp,
+      -- left disjunct is true
+        assume p,
+        exact p,
+      -- right disjunct is true
+        assume p,
+        exact p,
+  -- backwards
+    assume p, 
+    exact or.intro_left P p, /- or intor rule takes two arguments, a propostion and a proof-/
 end
+/- english language prooof:
+
+if p or p is true, then eiether p is true or p is true. 
+do case disjuction analysis on left side and on right side.-/
 
 example : ∀ (P : Prop), P ∧ P ↔ P := 
 begin
+  assume P,
+  apply iff.intro _ _,
+  -- forward
+    assume pandp,
+    apply and.elim_left pandp,
+  -- backward
+    assume p,
+    exact and.intro p p,
+
 end
 
 example : ∀ (P Q : Prop), P ∨ Q ↔ Q ∨ P := 
 begin
+  assume P Q,
+  apply iff.intro _ _,
+  -- forward
+    assume porq,
+    apply or.elim porq,
+    assume p,
+    apply or.intro_left 
+
+
 end
 
 example : ∀ (P Q : Prop), P ∧ Q ↔ Q ∧ P := 
 begin
+  assume P Q, 
+  apply iff.intro _ _,
+  -- forward
+    assume h,
+    apply and.intro _ _,
+    apply and.elim_right h,
+    apply and.elim_left h,
+  -- backward
+    assume h,
+    apply and.intro _ _,
+    apply and.elim_right h,
+    apply and.elim_left h,
 end
 
 example : ∀ (P Q R : Prop), P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (P ∧ R) := 
