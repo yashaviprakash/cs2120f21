48de6c82f04ad1778ecbfe13e092a15c5abcf8e0
Yashavi Prakash <yp3ez@virginia.edu>
2021-11-08 21:13:27 +0000

    .

--- src/instructor/lectures/lecture_25a.lean
+++ src/instructor/lectures/lecture_25a.lean
@@ -17,16 +17,25 @@ local infix `≺`:50 := r
 ORDERING RELATIONS ON A TYPE, β 
 -/
 
-def strict_ordering :=  asymmetric r ∧ transitive r
+def strict_ordering :=  asymmetric r ∧ transitive r -- asymmetric is stronger than antisymmetric, less than is an example
 
 
 def ordering :=         reflexive r ∧ transitive r ∧ anti_symmetric r
 
 
-def partial_order :=    reflexive r ∧ transitive r ∧ anti_symmetric r ∧ ¬strongly_connected r
+def partial_order :=    reflexive r ∧ transitive r ∧ anti_symmetric r ∧ ¬strongly_connected r -- not total
 
 
 def total_order :=      reflexive r ∧ transitive r ∧ anti_symmetric r ∧ strongly_connected r
 
+/-
+Well Order
+
+A relation r is a well ordering of a set if it's a total order, and if you pick
+any subset of a set on which you have a relation, any subset has to have a least 
+element
+-/
+
+def well_ordering := total_order r ∧ (∀ (s : set β), ∃ (b : β), ∀ (b' : β), b' ∈ s → b < b')
 end relation
 end relations
